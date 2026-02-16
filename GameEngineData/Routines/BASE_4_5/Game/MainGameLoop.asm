7691f4439aaaee4f2cb27c889c4f03426a23ed5f
kvanderburg <kevin.van.der.burg@gmail.com>
2026-02-16 15:24:03 +0100

    Minor refactoring of main game loop script

--- GameEngineData/Routines/BASE_4_5/Game/MainGameLoop.asm
+++ GameEngineData/Routines/BASE_4_5/Game/MainGameLoop.asm
@@ -1,70 +1,86 @@
-;;; main game loop.
 
+;; Main game loop
 MainGameLoop:
-	LDA vBlankTimer
-vBlankTimerLoop:
-	CMP vBlankTimer
-	BEQ vBlankTimerLoop
-	
 
-	LDA gameHandler
-	AND #%10000000
-	BEQ dontSkipNormalGameLoop
-		JMP skipNormalGameLoop
-	dontSkipNormalGameLoop:
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Handles box updates that need to be carried over a frame.		
-		LDA queueFlags
-		AND #%00000101
-		BNE doBoxUpdate
+    ;; Wait for NMI
+    LDA vBlankTimer
+    -
+        CMP vBlankTimer
+    BEQ -
+
+    ;; Check if game is in "normal game loop" mode
+    ;; If not, skip the normal game loop
+    LDA gameHandler
+    AND #%10000000
+    BEQ +
+        JMP +handleUpdateScreen
+    +
+
+	;; Handle box updates that need to be carried over a frame
+	LDA queueFlags
+	AND #%00000101
+	BNE +
 		JMP dontDoBoxUpdate
-	doBoxUpdate:
-		TXA
-		PHA
-		TYA
-		PHA
-		JSR doDrawBox
-		PLA
-		TAY
-		PLA
-		TAX
-		
-	dontDoBoxUpdate
-	
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; handles text updates that need to be carried over a frame.
-	LDA textHandler
-	BEQ noTextUpdates
-		JSR doDrawText
-noTextUpdates:
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;		
-		SwitchBank #$1A
-            JSR doHandleInputReads
-        ReturnBank
-		LDA #$00
-		STA spriteRamPointer
-		SwitchBank #$18
-			JSR doScreenPreDraw
-		ReturnBank
-		
-		JSR doHandleObjects
-		JSR doCamera 
-		;;;;;;;;;;;;;;;;;; SET GAME SCREEN
-		;;;;;; IN THE EVENT THERE IS NO ATTATCHED CAM SCRIPT TO DO IT
+    +
+    TXA
+    PHA
+    TYA
+    PHA
+    JSR doDrawBox
+    PLA
+    TAY
+    PLA
+    TAX
 
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		SwitchBank #$18
-			JSR doScreenPostDraw
-			.include SCR_HANDLE_GAME_TIMER
-		ReturnBank
-		JSR doCleanUpSpriteRam
-		;JSR doHandleUpdateCamera
-	skipNormalGameLoop:
-		JSR doHandleUpdateScreen
-		 SwitchBank #$1B
-			 JSR doHandleUpdateMusic
-			 
-			 
-		 ReturnBank
-	JMP MainGameLoop
+
+dontDoBoxUpdate:
+    
+	;; Handle text updates that need to be carried over a frame
+    LDA textHandler
+    BEQ +
+        JSR doDrawText
+	+
+
+	;; Read controller inputs
+    SwitchBank #$1A
+        JSR doHandleInputReads
+    ReturnBank
+
+    ;; Reset sprite RAM pointer
+    LDA #$00
+    STA spriteRamPointer
+
+    ;; Do screen pre-draw
+    SwitchBank #$18
+        JSR doScreenPreDraw
+    ReturnBank
+
+    ;; Handle objects (drawing, tile collisions, object collisions, ...)
+    JSR doHandleObjects
+
+    ;; Update camera
+    JSR doCamera 
+
+    ;; Do screen post-draw
+    SwitchBank #$18
+        JSR doScreenPostDraw
+        .include SCR_HANDLE_GAME_TIMER
+    ReturnBank
+
+    ;; Move unused sprites off screen
+    JSR doCleanUpSpriteRam
+
+
++handleUpdateScreen:
+
+	;; Do screen update (if needed)
+    JSR doHandleUpdateScreen
+
+    ;; Prepare music updates
+    SwitchBank #$1B
+		JSR doHandleUpdateMusic
+	ReturnBank
+
+	;; Repeat main game loop routine
+    JMP MainGameLoop
+
