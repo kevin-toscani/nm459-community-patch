e1bcf5413bc809e19a07ca8214d2ac4a45f1c7f3
kvanderburg <kevin.van.der.burg@gmail.com>
2026-02-16 18:06:38 +0100

    Optimized metroidvania varJump script

--- GameEngineData/Routines/BASE_4_5/Game/MOD_MetroidVania/Inputs/M_varJump.asm
+++ GameEngineData/Routines/BASE_4_5/Game/MOD_MetroidVania/Inputs/M_varJump.asm
@@ -1,18 +1,7 @@
 
-    LDX player1_object
-    LDA Object_v_speed_lo,x
-    CLC
-    ADC #$00
     LDA Object_v_speed_hi,x
-    ADC #$00
     BPL +skipJump
-        LDA Object_v_speed_hi,x
-        CMP #$01
-        BCC +skipJump
-
-        LDA #$00
-        SEC 
-        SBC #$01
+        LDA #$FF
         STA Object_v_speed_hi,x
     +skipJump:
 
